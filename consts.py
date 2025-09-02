99130d41332464986c2cab9949d52e49c4e894f9
voron <djhjyea@gmail.com>
2025-09-02 16:59:24 -0700

    add

---
+++ consts.py
@@ -0,0 +1,21 @@
+# Base Url
+API_URL = 'https://api.bitget.com'
+
+# http header
+CONTENT_TYPE = 'Content-Type'
+OK_ACCESS_KEY = 'ACCESS-KEY'
+OK_ACCESS_SIGN = 'ACCESS-SIGN'
+OK_ACCESS_TIMESTAMP = 'ACCESS-TIMESTAMP'
+OK_ACCESS_PASSPHRASE = 'ACCESS-PASSPHRASE'
+APPLICATION_JSON = 'application/json'
+OK_CHANNEL_API_CODE = 'X-CHANNEL-API-CODE'
+
+# header key
+LOCALE = 'locale'
+
+# method
+GET = "GET"
+POST = "POST"
+DELETE = "DELETE"
+
+REQUEST_PATH = '/user/verify'
